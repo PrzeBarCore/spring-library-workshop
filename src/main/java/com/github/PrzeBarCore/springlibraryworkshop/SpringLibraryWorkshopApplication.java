14f0b10d457a23383c4f517226378a2a8d595ec2
PrzeBarCore <korzonek2104@gmail.com>
2020-10-14 19:35:25 +0200

    first commit

---
+++ src/main/java/com/github/PrzeBarCore/springlibraryworkshop/SpringLibraryWorkshopApplication.java
@@ -0,0 +1,13 @@
+package com.github.PrzeBarCore.springlibraryworkshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLibraryWorkshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringLibraryWorkshopApplication.class, args);
+	}
+
+}
---
+++ src/main/resources/db/migration/V1__Create_Table_Authors.sql
@@ -0,0 +1,5 @@
+create table authors (
+    id int unsigned primary key auto_increment,
+    name varchar(20) not null,
+    last_name varchar(30)
+);
